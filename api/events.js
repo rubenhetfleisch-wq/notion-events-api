2fc8b6536b9eecf7f240383d9c9d36f2e3b512f2
rubenhetfleisch-wq <ruben.hetfleisch@aiaustria.com>
2026-01-02 14:37:02 +0100

    Update events.js

--- api/events.js
+++ api/events.js
@@ -1,1 +1,42 @@
+import { Client } from "@notionhq/client";
 
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
+  try {
+    const notion = new Client({
+      auth: process.env.NOTION_TOKEN
+    });
+
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DB_ID,
+      filter: {
+        property: "Published",
+        checkbox: { equals: true }
+      },
+      sorts: [
+        { property: "Date", direction: "ascending" }
+      ]
+    });
+
+    const items = response.results.map(page => ({
+      title: page.properties.Title?.title?.[0]?.plain_text ?? "",
+      desc: page.properties.Description?.rich_text?.[0]?.plain_text ?? "",
+      region: page.properties.Region?.select?.name ?? "",
+      url: page.properties.URL?.url ?? "",
+      image:
+        page.cover?.external?.url ||
+        page.cover?.file?.url ||
+        null,
+      date: {
+        start: page.properties.Date?.date?.start ?? null
+      }
+    }));
+
+    res.status(200).json({ items });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
